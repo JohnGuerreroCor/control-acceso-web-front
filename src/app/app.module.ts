0c5aaf2010503bccc0fb6ccc82c1ef9c6a0c4cad
JohnGuerreroCor <john.guerrero@usco.edu.co>
2024-01-16 14:54:18 -0500

    Ajustes login y token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,47 +4,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
-//ANGULAR MATERIAL
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatTableModule } from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatTreeModule } from '@angular/material/tree';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'
-import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MaterialModules } from './material.modules';
 
 //COMPONENTES
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { TokenComponent } from './components/token/token.component';
 
-
 //INICIO INTEGRACION FIREBASE PARA IMAGENES LINEALES EMAIL - REMPLAZO DE DATA URI BASE64
 
-import { environment } from "../environments/environment";
-import { AngularFireModule } from '@angular/fire/compat'
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { AngularFireAuthModule } from "@angular/fire/compat/auth";
-import { AngularFireStorageModule } from "@angular/fire/compat/storage";
-import { AngularFireAuthGuardModule } from "@angular/fire/compat/auth-guard";
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireAuthGuardModule } from '@angular/fire/compat/auth-guard';
 
 //FIN INTEGRACION FIREBASE PARA IMAGENES LINEALES EMAIL - REMPLAZO DE DATA URI BASE64
 
@@ -62,15 +36,21 @@ import { EstudianteComponent } from './components/estudiante/estudiante.componen
 //QR
 import { QRCodeModule } from 'angularx-qrcode';
 import { NgxPrintModule } from 'ngx-print';
-import { ZXingScannerModule } from "@zxing/ngx-scanner";
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { VirtualComponent } from './components/virtual/virtual.component';
 import { IntercambioComponent } from './components/intercambio/intercambio.component';
 import { PublicoComponent } from './components/publico/publico.component';
 import { DocenteComponent } from './components/docente/docente.component';
 import { AdministrativoComponent } from './components/administrativo/administrativo.component';
 import { GraduadoComponent } from './components/graduado/graduado.component';
-import { TiqueteInvitadosComponent, ModalTiqueteInvitado } from './components/tiquete/tiquete-invitados/tiquete-invitados.component';
-import { TiqueteVisitantesComponent, ModalTiqueteVisitante } from './components/tiquete/tiquete-visitantes/tiquete-visitantes.component';
+import {
+  TiqueteInvitadosComponent,
+  ModalTiqueteInvitado,
+} from './components/tiquete/tiquete-invitados/tiquete-invitados.component';
+import {
+  TiqueteVisitantesComponent,
+  ModalTiqueteVisitante,
+} from './components/tiquete/tiquete-visitantes/tiquete-visitantes.component';
 import { TiquetesComponent } from './components/inicio/tiquetes/tiquetes.component';
 import { EscanerComponent } from './components/escaner/escaner.component';
 import { EmailHidePipe } from './pipes/email-hide.pipe';
@@ -111,40 +91,15 @@ const initializer =
     ReactiveFormsModule,
     NgxPrintModule,
     ZXingScannerModule,
+    MaterialModules,
 
-     //INICIO FIREBASE
-     AngularFireModule.initializeApp(environment.firebase),
-     AngularFireDatabaseModule,
-     AngularFireAuthModule,
-     AngularFireStorageModule,
-     AngularFireAuthGuardModule,
-     //FIN FIREBASE
-
-     //INICIO ANGULAR MATERIAL
-    MatSidenavModule,
-    MatToolbarModule,
-    MatListModule,
-    MatButtonModule,
-    MatInputModule,
-    MatIconModule,
-    MatTabsModule,
-    MatSelectModule,
-    MatPaginatorModule,
-    MatDatepickerModule,
-    MatExpansionModule,
-    MatTreeModule,
-    MatCheckboxModule,
-    MatBottomSheetModule,
-    MatSlideToggleModule,
-    MatAutocompleteModule,
-    MatRadioModule,
-    MatFormFieldModule,
-    MatTableModule,
-    MatNativeDateModule,
-    MatGridListModule,
-    MatDialogModule,
-    MatCardModule,
-    //FIN ANGULAR MAERIAL
+    //INICIO FIREBASE
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFireAuthGuardModule,
+    //FIN FIREBASE
 
     AppRoutingModule,
     QRCodeModule,
@@ -152,9 +107,9 @@ const initializer =
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerWhenStable:30000',
     }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   entryComponents: [
     ModalTiqueteInvitado,
@@ -173,6 +128,6 @@ const initializer =
       multi: true,
     },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,18 @@
 export const environment = {
-    //COLOCAR EN FALSE LOCAL
-    production: false,
-    //DESCOMENTAR LOCAL
-    URL_BACKEND : "http://localhost:8080/puesto-vigilancia-1.0",
-    //DESCOMENTAR PRODUCCIÓN
-    //URL_BACKEND : "https://gaitana.usco.edu.co/control_acceso",
-    firebase: {
-      apiKey: 'AIzaSyDCI1DjDRTyWgCJd681nhrEqY216Kfbm44',
-      authDomain: 'doctoradocienciasdelasaludusco.firebaseapp.com',
-      databaseURL: 'https://doctoradocienciasdelasaludusco.firebaseio.com',
-      projectId: 'doctoradocienciasdelasaludusco',
-      storageBucket: 'doctoradocienciasdelasaludusco.appspot.com',
-      messagingSenderId: '399106387464',
-      appId: '1:399106387464:web:7741df6f88e489f7176bbd',
-      measurementId: 'G-PTHMQCDL1F'
-    }
+  //COLOCAR EN FALSE LOCAL
+  production: false,
+  //DESCOMENTAR LOCAL
+  URL_BACKEND: 'http://localhost:8080/control-acceso-web-1.0',
+  //DESCOMENTAR PRODUCCIÓN
+  //URL_BACKEND : "https://gaitana.usco.edu.co/control_acceso",
+  firebase: {
+    apiKey: 'AIzaSyDCI1DjDRTyWgCJd681nhrEqY216Kfbm44',
+    authDomain: 'doctoradocienciasdelasaludusco.firebaseapp.com',
+    databaseURL: 'https://doctoradocienciasdelasaludusco.firebaseio.com',
+    projectId: 'doctoradocienciasdelasaludusco',
+    storageBucket: 'doctoradocienciasdelasaludusco.appspot.com',
+    messagingSenderId: '399106387464',
+    appId: '1:399106387464:web:7741df6f88e489f7176bbd',
+    measurementId: 'G-PTHMQCDL1F',
+  },
 };
